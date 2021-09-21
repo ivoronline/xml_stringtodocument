2eb4fb429413dc6b2d267a6ea29deb7ebb8ee1ac
igoreta <ivoronline@gmail.com>
2021-09-21 14:55:05 +0200

    Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+public class Main {
+
+  //XML STRING
+  static String xmlString =
+    "<person Id='1'>\n" +
+    "  <name> John </name>\n" +
+    "  <age>  20   </age>\n" +
+    "</person>";
+
+  //================================================================================
+  // MAIN
+  //================================================================================
+  public static void main(String[] args) throws Exception {
+
+    //GET DOCUMENT
+    Document document = XMLUtil.stringToDocument(xmlString);
+
+    //GET AGE VALUE
+    Node     nameNode = document.getElementsByTagName("name").item(0);
+    String   name     = nameNode.getFirstChild().getNodeValue();
+
+    //DISPLAY AGE VALUE
+    System.out.println("Hello" + name);
+
+  }
+
+}
+
+
+
